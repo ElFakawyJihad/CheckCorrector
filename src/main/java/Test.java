2d71d919fcaf27608081c42cbd2ff6231d22cea4
Jihad <jihad394@hotmail.fr>
2016-10-01 01:04:33 +0200

    Add Test and Test1 for use Spoon

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,23 @@
+import java.util.Iterator;
+
+import spoon.Launcher;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
 
 public class Test {
+	 private static CtMethod<?> m;
 
+	public static void main(String[] args) {
+		Launcher l=new Launcher();
+		l.run();
+		CtClass<?> classe=l.getFactory().Class().get(Test1.class);
+		Iterator<CtMethod<?>> method=classe.getMethods().iterator();
+		while (method.hasNext()){
+			m=method.next();
+		}
+		classe.removeMethod(m);
+		l.getModelBuilder().compile();
+		
+		
+	}
 }
