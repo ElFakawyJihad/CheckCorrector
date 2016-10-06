8f2ae19a533d7c855855e7f5e1b5453d9af7713d
rbadr <rahal.badr@gmail.com>
2016-10-06 16:45:28 +0200

    Implements the git commands to Add/Commit/Push Files

--- src/main/java/PullRequestEngineering/PullRequestJGit.java
+++ src/main/java/PullRequestEngineering/PullRequestJGit.java
@@ -1,5 +1,48 @@
 package PullRequestEngineering;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.api.errors.*;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.Repository;
+
 public class PullRequestJGit {
     
+    private String localPath = "D:\\IAGL\\OPL\\PullRequestEngineeringTest";
+    private String remotePath = "https://user:mdp@github.com//rbadr/PullRequestEngineeringTest";
+    private Repository localRepo;
+    private Git git;
+    
+    public void gitInit() throws IOException {
+        localRepo = new FileRepository(localPath + "/.git");
+        git = new Git(localRepo);
+    }
+    
+    public void createRepo() throws IOException {
+        Repository newRepo = new FileRepository(localPath + ".git");
+        newRepo.create();
+    }
+    
+    public void cloneRepo() throws IOException, GitAPIException {
+        Git.cloneRepository().setURI(remotePath)
+                .setDirectory(new File(localPath)).call();
+    }
+    
+    public void addFile(String ValidFileName) throws IOException, GitAPIException {
+        File myfile = new File(localPath + "/" +ValidFileName);
+        myfile.createNewFile();
+        git.add().addFilepattern("myfile").call();
+    }
+    
+    public void commitChanges(String commitMessage) throws IOException, GitAPIException,
+            JGitInternalException {
+        git.commit().setMessage(commitMessage).call();
+    }
+    
+    public void pushCommit() throws IOException, JGitInternalException,
+            GitAPIException {
+        git.push().call();
+    }
 }
