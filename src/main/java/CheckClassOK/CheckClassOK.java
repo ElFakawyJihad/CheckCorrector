9e368d7384960a51fd8a542281fc13bd8b12e375
Jihad <jihad394@hotmail.fr>
2016-10-08 01:30:00 +0200

    Add File CheckClassOK

---
+++ src/main/java/CheckClassOK/CheckClassOK.java
@@ -0,0 +1,41 @@
+package CheckClassOK;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CheckClassOK {
+	private File unusedFile;
+	private String path;
+	private ArrayList<String> usedFileList;
+
+	public CheckClassOK(ArrayList<String> usedFileList, String path) throws IOException {
+		unusedFile = new File("FichierConforme.txt");
+		if (!unusedFile.exists()) {
+			unusedFile.createNewFile();
+		}
+		this.usedFileList = usedFileList;
+		this.path = path;
+	}
+
+	public void listFileUnused() throws IOException {
+		File folder = new File(path);
+		File[] file = folder.listFiles();
+		int length = file.length;
+		FileWriter fw = new FileWriter(unusedFile);
+		BufferedWriter bw = new BufferedWriter(fw);
+		String unused = "";
+
+		for (int i = 0; i < length; i++) {
+			String nameFile = file[i].getName();
+			if (!usedFileList.contains(nameFile)) {
+				unused = nameFile + "\n";
+			}
+		}
+		bw.write(unused);
+		bw.close();
+		fw.close();
+	}
+}
