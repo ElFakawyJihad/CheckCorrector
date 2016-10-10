b0d7570881ca1935a4b594ed5021e55354da8d11
Jihad <jihad394@hotmail.fr>
2016-10-10 20:56:02 +0200

    Modification de la methode listFileKO

--- src/main/java/CheckClassOK/CheckClassOK.java
+++ src/main/java/CheckClassOK/CheckClassOK.java
@@ -47,22 +47,15 @@ public class CheckClassOK {
 		File folder = new File(path);
 		File[] file = folder.listFiles();
 		int length = file.length;
-		FileWriter fw = new FileWriter(unusedFile);
-		BufferedWriter bw = new BufferedWriter(fw);
-		String unused = "";
 		File[] filesUnused = new File[usedFileList.size()];
 		int j = 0;
 		for (int i = 0; i < length; i++) {
 			String nameFile = file[i].getName();
 			if (usedFileList.contains(nameFile)) {
-				unused = nameFile + "\n";
 				filesUnused[j] = file[i];
 				j++;
 			}
 		}
-		bw.write(unused);
-		bw.close();
-		fw.close();
 		return filesUnused;
 	}
 }
