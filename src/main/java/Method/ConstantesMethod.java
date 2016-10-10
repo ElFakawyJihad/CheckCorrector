d6d72f673135135a857ae8c7713d4460920e7785
Jihad <jihad394@hotmail.fr>
2016-10-10 21:15:42 +0200

    Change Message

--- src/main/java/Method/ConstantesMethod.java
+++ src/main/java/Method/ConstantesMethod.java
@@ -2,7 +2,7 @@ package Method;
 
 public class ConstantesMethod {
 	public static final String DOCUMENTATION="Veillez ajouter la java doc";
-	public static final String MAJUSCULE="Pas de Majuscule dans le nom de Methode";
+	public static final String MAJUSCULE="Pas de Majuscule dans le nom de Methode,exemple : \"getName \" ";
 	public static final String LONGUEUR="Nom de Methode trop court";
 	public static final String TROPDELIGNE="Trop de ligne dans la methode";
 	public static final int NBLIGNE=10;
